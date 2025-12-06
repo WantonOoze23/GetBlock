2fc4d8158752eaa948df48b852e76caa84188a5b
Daniil <tyshko.daniil@lll.kpi.ua>
2025-12-06 04:04:03 +0200

    dev: refractored code, added client creation to di

--- app/src/main/java/com/tyshko/getblock/MainActivity.kt
+++ app/src/main/java/com/tyshko/getblock/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.lifecycle.ViewModelProvider
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.*
 import androidx.compose.ui.Modifier
@@ -19,9 +18,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-//        val getBlockModel = ViewModelProvider(this)[GetBlockViewModel::class.java]
-
         setContent {
             GetBlockTheme {
                 Surface(
--- app/src/main/java/com/tyshko/getblock/di/AppModule.kt
+++ app/src/main/java/com/tyshko/getblock/di/AppModule.kt
@@ -3,12 +3,33 @@ package com.tyshko.getblock.di
 import com.tyshko.getblock.data.repository.RpcRepository
 import com.tyshko.getblock.data.repository.RpcRepositoryImpl
 import com.tyshko.getblock.view.GetBlockViewModel
-import org.koin.core.module.dsl.bind
-import org.koin.core.module.dsl.singleOf
-import org.koin.core.module.dsl.viewModelOf
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.*
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
-    singleOf(::RpcRepositoryImpl){ bind<RpcRepository>() }
-    viewModelOf(::GetBlockViewModel)
+    single {
+        HttpClient {
+            install(ContentNegotiation) {
+                json(Json {
+                    encodeDefaults = true
+                    ignoreUnknownKeys = true
+                })
+            }
+            install(Logging) {
+                level = LogLevel.ALL
+                logger = Logger.DEFAULT
+            }
+        }
+    }
+    single<RpcRepository> {
+        RpcRepositoryImpl(get())
+    }
+    viewModel {
+        GetBlockViewModel(get())
+    }
 }
